a4f7d92a0a11b05dbf8c49c3b98f018a87d36b6d
Eric Muhren <eric.muhren@cz.nl>
2025-10-13 11:16:13 +0000

    aangepast

--- models/weerdata.sql
+++ models/weerdata.sql
@@ -1,1 +1,21 @@
-SELECT * FROM {{ source('weerdata', 'weerdata_gilze_rijen') }};
+WITH weerdata_transformed AS (
+    SELECT 
+        to_date(cast(yyyymmdd as string), 'yyyyMMdd') AS datum,
+        tx / 10.0 AS max_temperatuur,
+        tn / 10.0 AS min_temperatuur,
+        tg / 10.0 AS gemiddelde_temperatuur,
+        rh / 10.0 AS neerslag,
+        fg / 10.0 AS gemiddelde_windsnelheid
+    FROM 
+        {{ source('weerdata', 'weerdata_gilze_rijen') }}
+)
+
+SELECT 
+    datum,
+    max_temperatuur,
+    min_temperatuur,
+    gemiddelde_temperatuur,
+    neerslag,
+    gemiddelde_windsnelheid
+FROM 
+    weerdata_transformed
